26b014f073a27748574d96d581d84998a75d46a6
Yevhenii Onipko <onipko32@gmail.com>
2018-10-11 08:07:22 +0300

    deploy

--- client/src/domCoffeeConnect/index.js
+++ client/src/domCoffeeConnect/index.js
@@ -4,7 +4,7 @@ const domCoffeeConnection = (endpoint, request, responseProcessor) => {
   try {
     axios
       .request({
-        url: `https://dom-coffee-app.herokuapp.com${endpoint}`,
+        url: `http://dom-coffee-app.herokuapp.com${endpoint}`,
         method: request.method,
         headers: request.headers,
         data: request.data,
--- client/src/redux/actions/authentication.js
+++ client/src/redux/actions/authentication.js
@@ -11,7 +11,7 @@ export const setCurrentUser = decoded => ({
 
 export const registerUser = user => dispatch => {
   axios
-    .post('https://dom-coffee-app.herokuapp.com/users/register', user)
+    .post('http://dom-coffee-app.herokuapp.com/users/register', user)
     .then(() => history.push('/settings/users'))
     .catch(err => {
       dispatch({
@@ -23,7 +23,7 @@ export const registerUser = user => dispatch => {
 
 export const loginUser = user => dispatch => {
   axios
-    .post('https://dom-coffee-app.herokuapp.com/users/login', user)
+    .post('http://dom-coffee-app.herokuapp.com/users/login', user)
     .then(res => {
       const { token } = res.data;
       localStorage.setItem('jwtToken', token);
