f755719b9ca68a2e8436776ecee48dd7fedc60d6
Yevhenii Onipko <onipko32@gmail.com>
2019-01-02 09:26:18 +0200

    redeploy

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,10 +3,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
 /* eslint-disable */
-const enhancer = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const enhancer = compose(applyMiddleware(thunk));
 
 const store = createStore(rootReducer, enhancer);
 
