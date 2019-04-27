fadf6249a2f02952e298377004c339d831fbcd86
Onipko Jenya <onipkojenya@Onipkos-MacBook-Pro.local>
2019-04-27 08:32:32 +0300

    remove artifacts

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,8 +3,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
-const enhancer = compose(applyMiddleware(thunk),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = compose(applyMiddleware(thunk));
 
 const store = createStore(rootReducer, enhancer);
 
